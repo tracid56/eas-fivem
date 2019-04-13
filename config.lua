d4ab25bb3318a4e32cb99bb8b300f3509c9d8f08
Lance Good <lgoodyt@gmail.com>
2019-04-13 00:16:13 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,22 +1,3 @@
 Config = {}
 Config.EAS = {}
 Config.EAS.Volume = 0.2 --(0.2 = 20% Volume)
-Config.EAS.Departments = {
-    LSPD    = {
-    
-        name = "Los Santos Police Department"
-    
-    },
-    
-    LSSD    = {
-    
-        name = "Los Santos Sheriff's Department"
-    
-    },
-
-    USGVT   = {
-    
-        name = "United States Government"
-    
-    }
-}
