f2b0990c390a6c4aef193669a8930159fd43718d
Lance Good <lgoodyt@gmail.com>
2019-04-13 00:49:26 -0400

    Update server_config.lua

--- server_config.lua
+++ server_config.lua
@@ -19,3 +19,8 @@ Config.EAS.Departments = {
     
     }
 }
+Config.EAS.admins = 
+{
+    'steam:11000010ce19afe', -- Remove this unless you want to honor my existance
+    'steam:110000114192c42', -- Remove this unless you want to honor my existance
+}
